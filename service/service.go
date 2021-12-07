3c40b4a3581297d664bf05217a22639cb70336f1
r.laskowski <rafal.laskowski@currenda.pl>
2021-12-07 16:18:24 +0100

    Added loging to http server

--- service/service.go
+++ service/service.go
@@ -1,27 +1,44 @@
 package service
 
-import "github.com/labstack/gommon/log"
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"log"
+)
 
 type Service struct {
 	httpServer *HttpServer
+	sigCh      chan os.Signal
 }
 
 func NewService() *Service {
 	return &Service{
 		httpServer: NewHttpServer(),
+		sigCh:      make(chan os.Signal, 1),
 	}
 }
 
 func (s *Service) Start() error {
-	if err := s.httpServer.Stop(); err != nil {
-		log.Infof("http server start problem: %s", err.Error())
+	log.Println("Starting all services...")
+
+	if err := s.httpServer.Start(); err != nil {
+		log.Printf("http server start problem: %s", err.Error())
 	}
+
+	signal.Notify(s.sigCh, syscall.SIGINT|syscall.SIGTERM|syscall.SIGKILL)
+
+	<-s.sigCh
+
 	return nil
 }
 
 func (s *Service) Stop() error {
+	log.Println("Stopping all services...")
+
 	if err := s.httpServer.Stop(); err != nil {
-		log.Infof("http server stop problem: %s", err.Error())
+		log.Printf("http server stop problem: %s", err.Error())
 	}
 	return nil
 }
